7dbe132484372b2db7510792935f69aa64074610
la4zen <65774126+la4zen@users.noreply.github.com>
2020-11-10 04:04:42 +0300

    Added cardviev onclick listeners

--- app/src/main/java/com/la4zen/lolzapp/MainActivity.java
+++ app/src/main/java/com/la4zen/lolzapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.la4zen.lolzapp;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -7,7 +8,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import org.jsoup.Jsoup;
@@ -21,11 +24,11 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     public class Post {
-        public String title, text, author;
-        public void Create(String title, String text, String author) {
+        public String title, author, thread;
+        public void Create(String title, String author, String thread) {
             this.title = title;
-            this.text = text;
             this.author = author;
+            this.thread = thread;
         }
     }
 
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     public Context context;
     static ArrayList<Post> posts = new ArrayList<Post>();
     static String balance;
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,14 @@ public class MainActivity extends AppCompatActivity {
         mainLayout.setBackgroundColor(Color.GRAY);
         sharedPrefs = new SharedPrefs();
         sharedPrefs.init(context);
-
+        logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPrefs.setCookie(null);
+                logoutButton.setText("Для выхода из аккаунта перезайдите в приложение.");
+            }
+        });
         String cookie = SharedPrefs.getCookie();
         System.out.println(cookie);
         if (cookie == null) {
@@ -60,9 +71,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (data == null) {
-            return;
-        } new loadPage().execute();
+        new loadPage().execute();
     }
 
     @Override
@@ -81,8 +90,9 @@ public class MainActivity extends AppCompatActivity {
                 for (Element el : els) {
                     String author = el.select("span.username").get(0).text();
                     String title = el.select("span.spanTitle").get(0).text();
+                    String thread = el.select("a.listBlock.main.PreviewTooltip").attr("abs:href");
                     Post post = new Post();
-                    post.Create(title, "", author);
+                    post.Create(title, author, thread);
                     posts.add(post);
                 }
                 System.out.println("loaded");
@@ -111,10 +121,40 @@ public class MainActivity extends AppCompatActivity {
                     content.setPadding(15,15,15,15);
                     card.addView(content, params);
                     card.setRadius(50);
-
+                    card.setClickable(true);
+                    card.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // for future function
+                        }
+                    });
                     params.setMargins(20,10,20, 10);
                     mainLayout.addView(card, params);
                 }
+            }
+        }
+    }
+    public class loadThread extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... args) {
+            try {
+                Document doc = Jsoup.connect("https://lolz.guru/").header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                        .header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.310 (Edition Yx GX)")
+                        .cookie("xf_session", SharedPrefs.getCookie()).cookie("df_id", "960319fc0b468f741693392b329868f7").get();
+                Elements els = doc.select("div.discussionListItem");
+                for (Element el : els) {
+                    String author = el.select("span.username").get(0).text();
+                    String title = el.select("span.spanTitle").get(0).text();
+                    String thread = el.select("a.listBlock.main.PreviewTooltip").attr("abs:href");
+                    Post post = new Post();
+                    post.Create(title, author, "");
+                    posts.add(post);
+                }
+                System.out.println("loaded");
+                return null;
+            } catch (IOException e) {
+                System.out.println(e);
+                return "Произошла неизвестная ошибка.";
             }
         }
     }
